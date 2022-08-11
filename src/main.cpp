db98a1720ff6a135fb1b8c442a95b0af46d847c6
Aiden Kring <aidenjkring@gmail.com>
2022-08-11 08:44:34 -0400

    change to read circuit from file

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 
 #include "gates.hpp"
+#include "diagram.hpp"
+#include "parser.hpp"
 
 using namespace std;
 
@@ -9,21 +11,30 @@ using namespace std;
 
 
 
-int main(int argc, char **argv) {
-    LogicSource in1(1);
-    LogicSource in2(0);
-    NotGate reverse;
-    AndGate combine;
+int main() {
 
-    combine.input(&in1);
-    reverse.input(&in2);
-    combine.input(&reverse);
+    Circuit circuit;
+
+    parseFile("example.txt", &circuit);
+
+    for (signal output: circuit.outputs) {
+        cout << "value: " << output.value << ", voltage: " << output.voltage << endl;
+    }
+
+    // LogicSource in1(1);
+    // LogicSource in2(0);
+    // NotGate reverse;
+    // AndGate combine;
+
+    // combine.input(&in1);
+    // reverse.input(&in2);
+    // combine.input(&reverse);
 
 
-    cout << in1.output().value << " " << in2.output().value << endl;
-    cout << "| " << reverse.output().value  << endl;
-    cout << "\\ /" << endl;
-    cout << " " << combine.output().value << endl;
+    // cout << in1.output().value << " " << in2.output().value << endl;
+    // cout << "| " << reverse.output().value  << endl;
+    // cout << "\\ /" << endl;
+    // cout << " " << combine.output().value << endl;
 
     // cout << combine.output().value << endl;
 }
