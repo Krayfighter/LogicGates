a8f7053e3edf5a8037d445069380eeb9043afbf3
Aiden Kring <aidenjkring@gmail.com>
2022-08-28 09:30:55 -0400

    changed file format

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,8 @@
 
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <iostream>
 
 #include "gates.hpp"
 #include "diagram.hpp"
@@ -13,144 +15,178 @@ using namespace std;
 // circuit should have the celected component be the components vector
 // `lines` should point to an argument to the current action (creation of a component)
 // i is simply the current iteration number in the calling loop
-void linkComponent(Circuit *circuit, vector<string> *lines, unsigned int i) {
-    if (lines->at(i)[0] == '_') { // if the current line start with '_' it is handled as a circuit input
-        string snum = lines->at(i); // store the current line
-        string _snum; // output of following statements
-        for(unsigned int iter = 3; iter < snum.size(); iter++) { // start loop after the text of `_in`
-            _snum += snum[iter]; // add the current character (number character) to `_snum`
+template <typename componentType>
+size_t addComponent(Circuit *circuit, vector<string> *lines, size_t i) {
+    // ensure that template type is a subclass of ComponentBase
+    static_assert(is_base_of<ComponentBase, componentType>::value, "foreign class template used for addComopnent()");
+
+    // auto _lines = *lines;
+    size_t _i = i;
+    string name = lines->at(_i); // get the name of the component
+    circuit->components.insert( // insert the component
+        pair<string, ComponentBase *> ( // of pair type
+            name, // with name
+            new componentType(name) // to a new component type object
+        )
+    );
+    // *i--;
+    // i; // step backward
+
+    string type = lines->at(_i-1); // to get the gate type
+    _i++; // step forward to first input
+    if(type == "not") {
+        // _i++;
+        ComponentBase *input = circuit->components.at(lines->at(_i));
+        auto compointer = circuit->components.at(name);
+        // cout << type(compointer) << endl;
+        compointer->input(input);
+        // _i++;
+        return _i;
+    }else {
+        auto input2 = vector<string> {"and", "nand", "or", "nor", "xor"};
+        if (find(input2.begin(), input2.end(), type) != input2.end()) {
+            // _i++;
+            ComponentBase *input = circuit->components.at(lines->at(_i));
+            circuit->components.at(name)->input(input);
+            _i++;
+            input = circuit->components.at(lines->at(_i));
+            circuit->components.at(name)->input(input);
+            // _i++;
+            return _i;
+        }else {
+            throw runtime_error("unrecognized gate type entered");
         }
-        int inum = stoi(_snum);
-        circuit->components[ // get the latest member of circuit's components
-            circuit->components.size()-1
-        ].gate->input( // give its gate the input of previously obtained input number (snum)
-            circuit->inputs[inum].getValue()
-        );
-    }else { // the input to the current component is the output of another
-        LogicGate *gate = circuit->searchGate(lines->at(i)); // search for the input gate by name
-        circuit->components[ // get the current component
-            circuit->components.size()-1
-        ].gate->input(gate->output()); // set its input the the selected gate's output
     }
+
+    i++;
+    throw runtime_error("this point of the function should be unreachable");
 }
 
 // function that takes a filename and parses it into the given circuit pointer
-void parseFile(string filename, Circuit *circuit) {
+Circuit parseFile(string filename) {
+    Circuit circuit(filename, false);
     fstream filestream = fstream(filename);
     vector<string> lines;
     { // seperate variables and logic for less clutter
         string line;
-        string old;
-        filestream >> line;
-        while(line != old) {
-            lines.push_back(line);
-            old = line;
+        // filestream >> line;
+        do {
             filestream >> line;
-        }
+            lines.push_back(line);
+        }while(filestream.peek() != EOF);
     }
     // iterate through all the lines read by the file
-    for (unsigned int i = 0; i < lines.size(); i++) {
-        if (lines[i] == "inputs") {
-            i++;
-            circuit->inputCount = stoi(lines[i]);
-            // this increments the counter and sets the max number of inputs
-        }else if (lines[i] == "addin") {
+    for (size_t i = 0; i < lines.size(); i++) {
+        // if (lines[i] == "inputs") {
+        //     i++;
+        //     circuit->inputCount = stoi(lines[i]);
+        //     // this increments the counter and sets the max number of inputs
+        // }else 
+        if (lines[i] == "input") {
             i++;
             // check if circuit has available input space
-            if (circuit->inputs.size() < circuit->inputCount) {
+            // if (circuit->inputs.size() < circuit->inputCount) {
                 // set the value of the input the that of the next line read (1 or 0)
-                circuit->inputs.push_back(LogicSource(stoi(lines[i])));
-            }else {
-                // throw an error if more inputs are added than should be
-                throw runtime_error("Too many input values parsed");
-            }
+            // circuit->inputs.push_back(LogicSource(stoi(lines[i])));
+            string name = "_in"+lines[i];
+            i++;
+            circuit.components.insert( // insert a new component
+                pair<string, ComponentBase *>( // of pair type
+                    name,
+                    new Input(
+                        name,
+                        stoi(lines[i])
+                    )
+                )
+            );
+            // i++;
         }else if (lines[i] == "gate") {
             i++;
             if (lines[i] == "not") {
                 i++;
-                circuit->components.push_back(
-                    NamedLogicGate{
-                        lines[i],
-                        new NotGate()
-                    }
-                ); // adds a NotGate the the Circuit
-                i++;
-                linkComponent(circuit, &lines, i);
-                // increments the counter to the argument for this action
-                // then calls linkComponent with the circuit pointer, lines poinrter
-                // and the counter number
+                i = addComponent<NotGate>(&circuit, &lines, i);
+                // circuit.components[lines[]].
             }else if(lines[i] == "and") {
                 i++;
-                circuit->components.push_back(
-                    NamedLogicGate{
-                        lines[i],
-                        new AndGate()
-                    }
-                );
-                i++;
-                linkComponent(circuit, &lines, i);
-                i++;
-                linkComponent(circuit, &lines, i);
+                i = addComponent<AndGate>(&circuit, &lines, i);
+                // circuit->components.push_back(
+                //     NamedLogicGate{
+                //         lines[i],
+                //         new AndGate()
+                //     }
+                // );
+                // i++;
+                // linkComponent(circuit, &lines, i);
+                // i++;
+                // linkComponent(circuit, &lines, i);
             }else if(lines[i] == "nand") {
                 i++;
-                circuit->components.push_back(
-                    NamedLogicGate{
-                        lines[i],
-                        new NandGate()
-                    }
-                );
-                i++;
-                linkComponent(circuit, &lines, i);
-                i++;
-                linkComponent(circuit, &lines, i);
+                i = addComponent<NandGate>(&circuit, &lines, i);
+                // circuit->components.push_back(
+                //     NamedLogicGate{
+                //         lines[i],
+                //         new NandGate()
+                //     }
+                // );
+                // i++;
+                // linkComponent(circuit, &lines, i);
+                // i++;
+                // linkComponent(circuit, &lines, i);
             }else if(lines[i] == "or") {
                 i++;
-                circuit->components.push_back(
-                    NamedLogicGate{
-                        lines[i],
-                        new OrGate()
-                    }
-                );
-                i++;
-                linkComponent(circuit, &lines, i);
-                i++;
-                linkComponent(circuit, &lines, i);
+                i = addComponent<OrGate>(&circuit, &lines, i);
+                // i++;
+                // circuit->components.push_back(
+                //     NamedLogicGate{
+                //         lines[i],
+                //         new OrGate()
+                //     }
+                // );
+                // i++;
+                // linkComponent(circuit, &lines, i);
+                // i++;
+                // linkComponent(circuit, &lines, i);
             }else if(lines[i] == "nor") {
                 i++;
-                circuit->components.push_back(
-                    NamedLogicGate{
-                        lines[i],
-                        new NorGate()
-                    }
-                );
-                i++;
-                linkComponent(circuit, &lines, i);
-                i++;
-                linkComponent(circuit, &lines, i);
+                i = addComponent<NorGate>(&circuit, &lines, i);
+                // circuit->components.push_back(
+                //     NamedLogicGate{
+                //         lines[i],
+                //         new NorGate()
+                //     }
+                // );
+                // i++;
+                // linkComponent(circuit, &lines, i);
+                // i++;
+                // linkComponent(circuit, &lines, i);
             }else if(lines[i] == "xor") {
                 i++;
-                circuit->components.push_back(
-                    NamedLogicGate{
-                        lines[i],
-                        new XorGate()
-                    }
-                );
-                i++;
-                linkComponent(circuit, &lines, i);
-                i++;
-                linkComponent(circuit, &lines, i);
+                i = addComponent<XorGate>(&circuit, &lines, i);
+                // circuit->components.push_back(
+                //     NamedLogicGate{
+                //         lines[i],
+                //         new XorGate()
+                //     }
+                // );
+                // i++;
+                // linkComponent(circuit, &lines, i);
+                // i++;
+                // linkComponent(circuit, &lines, i);
             }else {
                 throw runtime_error("Invalid gate name parsed");
             }
         }else if (lines[i] == "output") {
             i++;
-            // increment the counter to point to this actions argument
-            // then search for it in the circuits components vector
-            LogicGate *gate = circuit->searchGate(lines[i]);
-            // then push its output onto the circuits output vector
-            circuit->outputs.push_back(gate->output());
+            // circuit
+            // i++;
+            string tmp = lines[i];
+            circuit.input(
+                circuit.components.at(lines[i])
+            );
+            // i++;
         }else {
             throw runtime_error("invalid statement parsed");
         }
     }
+    return circuit;
 }
