7f8656a04a2a822d1f1fec85111dd38d92f72c47
Aiden Kring <aidenjkring@gmail.com>
2022-08-28 21:58:21 -0400

    package the resulting build files

--- cmake.sh
+++ cmake.sh
@@ -1,5 +1,20 @@
+# folder for organized build output
+mkdir bin
+
+# build with cmake
 cmake . -B build
 cd build
 make
-cd src
+
+# move the build products into the bin directory
+cp src/LogicGates ../bin/LogicGates
+cd ..
+cp src/example.txt bin/example.txt
+
+# create the release tarball
+tar -cjf linux.tar.bz bin
+
+
+# test the app
+cd bin
 ./LogicGates
